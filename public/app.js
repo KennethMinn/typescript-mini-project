bbb0ca08c08b59a84141264efb72d01ab4407178
KennethMinn <minthukha2472005@gmail.com>
2023-04-28 10:12:43 +0630

    initialized class and added access modifiers

--- public/app.js
+++ public/app.js
@@ -1,4 +1,27 @@
 "use strict";
+class Invoice {
+    // client: string;
+    // details: string;
+    // amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    method() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+        // console.log(`${this.client} owes ${this.amount} for ${this.details}`);
+    }
+}
+const invOne = new Invoice("mtk", "working with mfs", 500);
+const invTwo = new Invoice("st", "working with mfs", 400);
+console.log(invOne, invTwo);
+console.log(invOne.method(), invTwo.method());
+let invoices = []; // can only push things that are inside of Invoice class because Invoice[] type
+invoices.push(invOne, invTwo);
 const anchor = document.querySelector("a");
 console.log(anchor.href);
 const form = document.querySelector(".new-item-form");
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,33 @@
-const anchor = document.querySelector("a")!;
+class Invoice {
+  // client: string;
+  // details: string;
+  // amount: number;
 
+  constructor(
+    public client: string,
+    public details: string,
+    readonly amount: number
+  ) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  method() {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+    // console.log(`${this.client} owes ${this.amount} for ${this.details}`);
+  }
+}
+
+const invOne = new Invoice("mtk", "working with mfs", 500);
+const invTwo = new Invoice("st", "working with mfs", 400);
+console.log(invOne, invTwo);
+console.log(invOne.method(), invTwo.method());
+
+let invoices: Invoice[] = []; // can only push things that are inside of Invoice class because Invoice[] type
+invoices.push(invOne, invTwo);
+
+const anchor = document.querySelector("a")!;
 console.log(anchor.href);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
